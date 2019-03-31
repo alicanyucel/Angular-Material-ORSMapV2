c7ca5691c865f4ff10b24a435879684cf526b76c
Kirk Clawson <kirk.clawson@outlook.com>
2019-03-31 00:10:58 -0400

    Hook up service API

--- src/app/isochrone-ui/isochrone-ui.component.ts
+++ src/app/isochrone-ui/isochrone-ui.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-isochrone-ui',
@@ -12,4 +13,24 @@ export class IsochroneUiComponent implements OnInit {
   ngOnInit() {
   }
 
+  getResults() {
+    // @ts-ignore
+    const isochrones = new Openrouteservice.Isochrones({
+      api_key: environment.apiKey
+    });
+    isochrones.calculate({
+      locations: [[-83.4255176, 42.432238]], // these are in long/lat pairs, not lat/long pairs.
+      location_type: 'start',
+      profile: 'driving-car',
+      range: 900, // time in seconds
+      interval: [300], // this number is used to split up the above number into regions
+      range_type: 'time'
+    })
+      .then((response) => {
+        console.log('response', response);
+      })
+      .catch((err) => {
+        console.log('An error occurred: ', err);
+      });
+  }
 }
